caa6fed68eb7a6bc7d54def4b67bdc4be6f1ad73
Amanda Fonseca <amandafj.estudante@gmail.com>
2024-03-11 21:13:31 -0300

    Exercicio finalizado em aula.

---
+++ Exercicio3.java
@@ -0,0 +1,29 @@
+/**Escreva um programa que calcule o teorema de Pitágoras, representado pela expressão: c =(raiz quadrada) a2+b2 
+ */
+
+import java.util.Scanner;
+
+public class Exercicio3 {
+    public static void main (String[]args) {
+    Scanner input = new Scanner(System.in);
+    double c;
+    double a;
+    double b;
+    double raiz;
+
+    System.out.println("Digite o primeiro número: ");
+    a = input.nextDouble();
+    
+    System.out.println("Digite o segundo número: ");
+    b = input.nextDouble();
+
+    raiz = (a*a) + (b*b);
+    
+    c = Math.sqrt (raiz);
+    double raizResul = Math.ceil(c);
+
+    System.out.println("A expressão c =(raiz quadrada) a2+b2 deu o resultado arredondado de: " + raizResul );
+   
+    input.close();
+}
+}
